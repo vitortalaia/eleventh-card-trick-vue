641eb4c8054fa3048c029de3dbe722c388a8e91e
Vitor Talaia <vitor.talaia.oliveira@gmail.com>
2017-11-02 16:59:21 -0200

    Add landing page component
    This component will be the first one that a person sees when accessing the
    application

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,18 @@ export default {
   name: 'app'
 }
 </script>
+
+<style>
+html,
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  height: 100vh;
+  font-family: 'Raleway', sans-serif;
+  font-size: 16px;
+  color: #fff;
+  background-color: #b9514e;
+}
+</style>
