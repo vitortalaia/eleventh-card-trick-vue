f965460b6a7f29c6bcf188a4f0114f36964ffb30
Vitor Talaia <vitor.talaia.oliveira@gmail.com>
2017-11-02 21:24:45 -0200

    Create a component to hold instructions based on the current round

---
+++ src/components/RoundInstructions.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>{{ instruction }}</p>
+</template>
+
+<script>
+export default {
+  name: 'round-instructions',
+
+  props: ['currentRound'],
+
+  computed: {
+    instruction () {
+      return this.instructions[this.currentRound]
+    }
+  },
+
+  data () {
+    return {
+      instructions: [
+        `Choose a card, remember it, and click on the button that corresponds to
+        the row on which your card is.`,
+
+        `Now, click on the row that the card you've previously chosen is.`,
+
+        `One last time! Click on the row on which your card is.`
+      ]
+    }
+  }
+}
+</script>
+
--- src/pages/Trick.vue
+++ src/pages/Trick.vue
@@ -2,10 +2,7 @@
   <div class="container">
     <h1 class="title">Eleventh Card Trick</h1>
 
-    <p>
-      Choose a card, remember it, and click on the button that corresponds to
-      the row on which your card is.
-    </p>
+    <RoundInstructions :currentRound="currentRound" />
 
     <Spinner v-if="isFetching" />
 
@@ -29,17 +26,23 @@ import * as actions from '@/store/action-types'
 
 import Spinner from '@/components/Spinner'
 import CardsTable from '@/components/CardsTable'
+import RoundInstructions from '@/components/RoundInstructions'
 
 export default {
   name: 'trick',
 
-  components: { Spinner, CardsTable },
+  components: {
+    Spinner,
+    CardsTable,
+    RoundInstructions
+  },
 
   computed: {
     ...mapState([
       'numberOfRows',
       'cardsPerRow',
-      'isFetching'
+      'isFetching',
+      'currentRound'
     ]),
 
     ...mapGetters(['columns'])
